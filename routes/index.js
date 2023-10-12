9716107e1939edb6bf7ef2a7ae6ac044d3d42299
root <root@95-163-242-71.cloudvps.regruhosting.ru>
2023-10-12 09:53:49 +0300

    add new route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,23 @@
 const router = require('express').Router()
 const version = require('../package.json').version
+const db = require('../utils/db.js')
 
 // api routes
 // check health route
 router.get('/health', async (req, res) => {
   return res.status(200).json({ success: true, version, msg: 'Сервис работает стабильно' })
+})
+
+//get guests
+router.get('/guests', async (req, res) => {
+  try {
+    const [guests] = await db.query('select id, guest, invite_msg, additional_guest, guest_count, tel_number, status, mddate from guests;');
+    const [guestCount] = await db.query('select sum(guest_count) from guests');
+    return res.status(200).json({success: true, data: guests, count: guests.length, guestCount});
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({msg: 'Internal Error', error});
+  }
 })
 
 // 404 not found
